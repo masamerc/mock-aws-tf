9e77e922ea3843ef423e028feaa5b7de143f9b47
Masa Fukui <masahiro.m.fukui@gmail.com>
2024-12-29 10:01:41 +0900

    add moved example

--- s3.tf
+++
@@ -1,3 +0,0 @@
-resource aws_s3_bucket "another_bucket" {
-  bucket = "another-bucket"
-}
---
+++ service.tf
@@ -0,0 +1,54 @@
+module "main" {
+  source = "./modules/"
+}
+
+# moved
+
+moved {
+  from = aws_s3_bucket.another_bucket
+  to = module.main.aws_s3_bucket.another_bucket
+}
+
+moved {
+  from = aws_s3_bucket.the-accounts
+  to = module.main.aws_s3_bucket.the-accounts
+}
+
+moved {
+  from = aws_vpc.main
+  to = module.main.aws_vpc.main
+}
+
+
+# resource aws_s3_bucket "another_bucket" {
+#   bucket = "another-bucket"
+# }
+#
+# locals {
+#   account_maps = {
+#     todd = "111111111111"
+#     james = "222222222222"
+#     alice = "333333333333"
+#     dottie = "444444444444"
+#   }
+# }
+#
+# resource aws_s3_bucket "the-accounts" {
+#   for_each = local.account_maps
+#   bucket   = "${each.key}-bucket-${each.value}"
+# }
+#
+# locals {
+#   cidrs = [
+#     "244.178.0.0/16",
+#     "10.0.0.0/16",
+#     "110.0.0.0/16",
+#   ]
+# }
+#
+#
+# resource "aws_vpc" "main" {
+#   for_each = toset(local.cidrs)
+#   cidr_block = each.value # key and value are the same for sets
+#   instance_tenancy = "default"
+# }
